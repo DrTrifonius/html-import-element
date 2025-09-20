b5f7f181dea3b018fce9db8d886ce05d5614fadd
Jonas 'Trifonius <etherealcommander@gmail.com>
2025-09-20 17:18:38 +0200

    added example

--- html-import-element.js
+++ html-import-element.js
@@ -1,11 +1,9 @@
 class HTMLImport extends HTMLElement {
 	async connectedCallback() {
 		// src should contain a relative link to the HTML file
-		const htmlSrc = this.getAttribute('src');
+		const htmlSrc = this.getAttribute("src");
 		// root indicates the element whose children should be cloned (e.g. body)
-		const rootSelector = this.getAttribute('root')
-
-
+		const rootSelector = this.getAttribute("root");
 
 		// fetch the content
 		const htmlResult = await fetch(htmlSrc);
@@ -18,16 +16,18 @@ class HTMLImport extends HTMLElement {
 
 		// create an empty element to store new DOM
 		//09/2025: changed from template to body to facilitate root selection
-		const domHolder = document.createElement('body');
+		const domHolder = document.createElement("body");
 
 		domHolder.innerHTML = htmlContent;
 
 		// if any script tags are added, clone them
-		const scripts = domHolder.querySelectorAll('script');
+		const scripts = domHolder.querySelectorAll("script");
 		scripts.forEach((script) => {
 			// Clone the script node
-			const clonedScript = document.createElement('script');
-			[...script.attributes].forEach((attr) => clonedScript.setAttribute(attr.name, attr.value));
+			const clonedScript = document.createElement("script");
+			[...script.attributes].forEach((attr) =>
+				clonedScript.setAttribute(attr.name, attr.value)
+			);
 			clonedScript.textContent = script.textContent;
 
 			// replace the original script tag with this new one (which will cause it to trigger)
@@ -35,13 +35,16 @@ class HTMLImport extends HTMLElement {
 		});
 
 		// replace this tag with the new content
-		if(rootSelector){
-			this.replaceWith(...domHolder.querySelector(rootSelector).childNodes)
-		}
-		else{
-			this.replaceWith(...domHolder.childNodes)
+		if (
+			rootSelector &&
+			//outer html and body tags get dropped automatically, as neither can be contained by a <body> tag, so they would crash
+			!(rootSelector === "body" || rootSelector === "html")
+		) {
+			this.replaceWith(...domHolder.querySelector(rootSelector).childNodes);
+		} else {
+			this.replaceWith(...domHolder.childNodes);
 		}
 	}
 }
 
-customElements.define('html-import', HTMLImport)
+customElements.define("html-import", HTMLImport);
